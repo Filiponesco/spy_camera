772d1e149b3453501c62f028de4f5a61d8ab9422
Filip <fifigaw98@gmail.com>
2020-04-30 13:39:38 +0200

    add provider but too much code

--- lib/camera_management.dart
+++ lib/camera_management.dart
@@ -1,34 +1,130 @@
-import 'dart:io';
+import 'dart:async';
 import 'package:camera/camera.dart';
+<<<<<<< Updated upstream
 import 'package:path_provider/path_provider.dart';
+=======
+import 'package:downloads_path_provider/downloads_path_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:vibration/vibration.dart';
+>>>>>>> Stashed changes
 // singleton
-class CameraManagement{
+//notify isRecording, which camera
+class CameraManagement extends ChangeNotifier{
   static final CameraManagement _cameraManagement = CameraManagement._internal();
+<<<<<<< Updated upstream
   factory CameraManagement(){
+=======
+  factory CameraManagement() {
+    _cameraManagement._loadCameras();
+    _cameraManagement._loadPreferences();
+    print("Constructor");
+>>>>>>> Stashed changes
     return _cameraManagement;
   }
   CameraManagement._internal();
 
+  bool _isLoadCameras = false;
+  bool _isLoadPreferences = false;
+  bool _isSetupCameras = false;
+  bool _isRecording = false;
+  bool _isPause = false;
+  Function(String message) _informUICallback;
   CameraController controller;
+<<<<<<< Updated upstream
   bool isReady = false;
   bool isRecording = false;
+=======
+  SharedPreferences _prefs;
+  CameraPosition _camPos = CameraPosition.back;
+  List<CameraDescription> _cameras;
+>>>>>>> Stashed changes
 
-  Future<void> setupCameras() async {
+  set isRecording(bool value){
+    this._isRecording = value;
+    print("Notify is RECORDING");
+    notifyListeners();
+  }
+  bool get isRecording{
+    return this._isRecording;
+  }
+  set cameraPosition(CameraPosition value){
+    this._camPos = value;
+    notifyListeners();
+  }
+  CameraPosition get cameraPosition{
+    return this._camPos;
+  }
+  bool get isReadyToSetup{
+    if(_isLoadCameras && _isLoadPreferences && !isRecording)
+      return true;
+    else
+      return false;
+  }
+  bool get isReadyToRecord{
+    if(_isLoadCameras && _isLoadPreferences && _isSetupCameras && !isRecording)
+      return true;
+    else
+      return false;
+  }
+  set informUI (Function(String) value){
+    this._informUICallback = value;
+    notifyListeners();
+  }
+  Future<void> _loadCameras() async{
     try{
-      if(!isReady){
-        List<CameraDescription> cameras = await availableCameras();
-        controller = CameraController(cameras[0], ResolutionPreset.medium);
+        _cameras = await availableCameras();
+        _isLoadCameras = true;
+        print("Load cameras");
+    } on CameraException catch(e){
+      print("Load cameras: $e");
+      _showAndThrowCameraException(e);
+      return null;
+    } catch(e){
+      print("Big error, load cameras $e");
+      return null;
+    }
+  }
+  Future<void> _loadPreferences() async{
+    try{
+      _prefs = await SharedPreferences.getInstance();
+      _isLoadPreferences = true;
+      print("Load preferences");
+    } catch(e){
+      print("Big error, load preferences: $e");
+      return null;
+    }
+  }
+  Future<void> _setupCameras() async {
+    try{
+      if(isReadyToSetup) {
+        controller =
+            CameraController(_cameras[cameraPosition.index], ResolutionPreset.medium);
         await controller.initialize();
+<<<<<<< Updated upstream
         isReady = true;
       }
     } on CameraException catch(e){
         _showCameraException(e);
         return null;
+=======
+        _isSetupCameras = true;
+        print("setup cameras");
+      }
+    } on CameraException catch(e){
+      _showAndThrowCameraException(e);
+      return null;
+    } catch(e){
+      print("Big error, setup cameras: $e");
+      return null;
+>>>>>>> Stashed changes
     }
-    print("setup cameras");
   }
   Future<void> startVideoRecording() async{
     try{
+<<<<<<< Updated upstream
       if(!isRecording) {
         String path = await _getVideoPath;
         await controller.startVideoRecording(path);
@@ -37,7 +133,57 @@ class CameraManagement{
       }
     } on CameraException catch(e){
       _showCameraException(e);
+=======
+      await _setupCameras();
+      if(isReadyToRecord) {
+        if(_prefs.getBool('vibration_start') != null) {
+          if (_prefs.getBool('vibration_start')) {
+            _vibrate(50);
+          }
+        }
+        String path = await _getVideoPath;
+        await controller.startVideoRecording(path);
+        isRecording = true;
+        print("Start recording");
+        _informUICallback("Recording...");
+      }
+    } on CameraException catch(e){
+      _showAndThrowCameraException(e);
+      print("CameraExcp $e");
+>>>>>>> Stashed changes
       return null;
+    }
+  }
+  Future<void> pauseVideoRecording() async{
+    try{
+      if(!controller.value.isRecordingVideo){
+        return null;
+      }
+        controller.pauseVideoRecording();
+        _isPause = true;
+    } on CameraException catch(e){
+      print("pause video $e");
+      _showAndThrowCameraException(e);
+    } catch(e){
+      print("Big error, pause video recording $e");
+    }
+  }
+  Future<void> resumeVideoRecording() async{
+    try{
+      if(!controller.value.isRecordingVideo){
+        return null;
+      }
+      if(_isPause){
+        controller.resumeVideoRecording();
+        _isPause = false;
+      }
+      else
+        return null;
+    } on CameraException catch(e){
+      print("resume video $e");
+      _showAndThrowCameraException(e);
+    } catch(e){
+      print("Big error, resume video recording $e");
     }
   }
   Future<void> stopVideoRecording() async {
@@ -59,6 +205,22 @@ class CameraManagement{
   }
   void _showCameraException(CameraException e){
     print("${e.code} ${e.description}");
+<<<<<<< Updated upstream
+=======
+    throw e;
   }
-
+  void _vibrate(int duration) async {
+    if (await Vibration.hasVibrator()) {
+      Vibration.vibrate(duration: duration);
+    }
+    else {
+      print("Vibrator no mounted on device");
+      _showAndThrowCameraException(CameraException("Vibrator", "Vibrator no mounted on device"));
+    }
+>>>>>>> Stashed changes
+  }
+}
+enum CameraPosition{
+  back,
+  front
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
+=======
+import 'package:camera/camera.dart';
+import 'package:camera/new/camera.dart';
+>>>>>>> Stashed changes
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,7 +21,8 @@ void main() {
   runApp(
     TimerServiceProvider(
       service: timerService,
-      child: MyApp(),
+      child: ChangeNotifierProvider(
+          create: (context) => CameraManagement(), child: MyApp()),
     ),
   );
 }
@@ -39,11 +45,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+<<<<<<< Updated upstream
   CameraManagement _cam;
   @override
   void initState() {
     super.initState();
     _cam = new CameraManagement();
+=======
+  //CameraManagement _cam;
+  bool CamIsRunning = false;
+  bool MicIsRunning = false;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  String title, text;
+  final Map<CameraPosition, String> _nameCam = {
+    CameraPosition.back: "Back camera",
+    CameraPosition.front: "Front camera"
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    print("INIT STATE");
+    //_cam = new CameraManagement(_showToast);
+    /*_cam.setupCameras().catchError(
+        (error) => _showAlertDialog("Camera ${error.code}", error.description));*/
+>>>>>>> Stashed changes
     flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
     var android = new AndroidInitializationSettings('@mipmap/ic_launcher');
     var iOS = new IOSInitializationSettings();
@@ -58,10 +84,37 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     });
   }
 
+<<<<<<< Updated upstream
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
   String title, text;
   final titleController = TextEditingController();
   final textController = TextEditingController();
+=======
+  showNotification(int id, String title, String text) async {
+    title = await getSortingOrder('notification_title');
+    text = await getSortingOrder('notification_description');
+    var android = new AndroidNotificationDetails(
+        'channel id', 'channel NAME', 'CHANNEL DESCRIPTION',
+        priority: Priority.High, importance: Importance.Max);
+    var iOS = new IOSNotificationDetails();
+    var platform = new NotificationDetails(android, iOS);
+    await flutterLocalNotificationsPlugin.show(id, title, text, platform,
+        payload: 'payload');
+  }
+
+  hideNotification() async {
+    var android = new AndroidNotificationDetails(
+        'channel id', 'channel NAME', 'CHANNEL DESCRIPTION',
+        priority: Priority.High, importance: Importance.Max);
+    var iOS = new IOSNotificationDetails();
+    var platform = new NotificationDetails(android, iOS);
+    await flutterLocalNotificationsPlugin.cancel(0);
+  }
+
+  Future onSelectNotification(String payload) {
+    _stopRecordTimeNotifyOrShowError();
+  }
+>>>>>>> Stashed changes
 
   Future<String> getSortingOrder(String type) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -78,6 +131,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     return prefs.setString(type, value);
   }
 
+<<<<<<< Updated upstream
   showNotification(int id, String title, String text) async {
     var android = new AndroidNotificationDetails(
         'channel id', 'channel NAME', 'CHANNEL DESCRIPTION',
@@ -98,12 +152,116 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         content: new Text('Notyfication click'),
       ),
     );*/
+=======
+  void _showToast(String message) {
+    Toast.show(message, context,
+        duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+  }
+
+  void _showAlertDialog(String title, String message) {
+    showDialog(
+        context: context,
+        builder: (context) => GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: AlertDialog(
+                title: Text(title),
+                content: Text(message),
+              ),
+            ));
+  }
+
+  void _startRecordTimeNotifyOrShowError() async {
+    try {
+      var camStatus = Provider.of<CameraManagement>(context, listen: false);
+      await camStatus.startVideoRecording();
+      var timerService = TimerService.of(context);
+      timerService.start();
+      showNotification(0, title, text);
+      CamIsRunning = true;
+    } on CameraException catch (error) {
+      _showAlertDialog(error.code, error.description);
+    }
+  }
+  void _pauseRecordTimeOrShowError() async {
+    try {
+      var camStatus = Provider.of<CameraManagement>(context, listen: false);
+      await camStatus.pauseVideoRecording();
+      var timerService = TimerService.of(context);
+      if (timerService.isRunning) {
+        CamIsRunning = true;
+        timerService.stop();
+      }
+    } on CameraException catch (error) {
+      _showAlertDialog(error.code, error.description);
+    }
+  }
+  void _resumeRecordTimeOrShowError() async{
+    try {
+      var camStatus = Provider.of<CameraManagement>(context, listen: false);
+      await camStatus.resumeVideoRecording();
+      var timerService = TimerService.of(context);
+      if (timerService.isRunning) {
+        CamIsRunning = true;
+        timerService.start();
+      }
+    } on CameraException catch (error) {
+      _showAlertDialog(error.code, error.description);
+    }
+  }
+  void _stopRecordTimeNotifyOrShowError() async {
+    try {
+      var camStatus = Provider.of<CameraManagement>(context, listen: false);
+      await camStatus.stopVideoRecording();
+      var timerService = TimerService.of(context);
+      if (timerService.isRunning) {
+        CamIsRunning = true;
+        timerService.stop();
+      }
+      hideNotification();
+    } on CameraException catch (error) {
+      _showAlertDialog(error.code, error.description);
+    }
+>>>>>>> Stashed changes
+  }
+
+  void _onClickChangeCamera() {
+    var camStatus = Provider.of<CameraManagement>(context, listen: false);
+    switch (camStatus.cameraPosition) {
+      case CameraPosition.back:
+        camStatus.cameraPosition = CameraPosition.front;
+        break;
+      case CameraPosition.front:
+        camStatus.cameraPosition = CameraPosition.back;
+        break;
+    }
+  }
+
+  void _onClickStartPause() {
+    var camStatus = Provider.of<CameraManagement>(context, listen: false);
+    if (camStatus.isRecording) {
+      _pauseRecordTimeOrShowError();
+    } else if(camStatus.controller.value.isRecordingPaused){
+        _resumeRecordTimeOrShowError();
+    } else{
+      _startRecordTimeNotifyOrShowError();
+    }
+  }
+  void _onClickStop(){
+    var timerService = TimerService.of(context);
+    var camStatus = Provider.of<CameraManagement>(context, listen: false);
+    if (camStatus.isRecording || camStatus.controller.value.isRecordingPaused) {
+      _stopRecordTimeNotifyOrShowError();
+      timerService.reset();
+      hideNotification();
+      CamIsRunning = false;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     var timerService = TimerService.of(context);
 
+<<<<<<< Updated upstream
     return DefaultTabController(
       length: 2,
       child: new Scaffold(
@@ -210,12 +368,73 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
             ),
           ),
         ]),
+=======
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Spy Camera"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            tooltip: 'Settings',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsRoute()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Consumer<CameraManagement>(
+        builder: (context, _camStatus, child) {
+          //_camStatus.controller.
+          //like callback
+          _camStatus.informUI = _showToast;
+          return Column(children: <Widget>[
+            Text(_nameCam[_camStatus.cameraPosition]),
+            RaisedButton(
+                child: Text("Change camera"),
+                onPressed:
+                _camStatus.isRecording ? null : () => _onClickChangeCamera()),
+            SizedBox(height: 30),
+            Center(
+              child: AnimatedBuilder(
+                animation: timerService,
+                builder: (context, child) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                          'Cam time: ${timerService.currentDuration.inHours %
+                              60}h : ${timerService.currentDuration.inMinutes %
+                              60}m : ${timerService.currentDuration.inSeconds %
+                              60}s'),
+                      RaisedButton(
+                        onPressed: () => _onClickStartPause(),
+                        child: Text(
+                            !timerService.isRunning ? 'START' : 'PAUSE'),
+                      ),
+                      RaisedButton(
+                        onPressed: () {
+                          _onClickStop();
+                        },
+                        child: Text('STOP'),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ]);
+        }
+>>>>>>> Stashed changes
       ),
     );
   }
   @override
   void dispose() {
+    var camStatus = Provider.of<CameraManagement>(context, listen: false);
     super.dispose();
-    _cam.controller.dispose();
+    camStatus.controller.dispose();
   }
 }
